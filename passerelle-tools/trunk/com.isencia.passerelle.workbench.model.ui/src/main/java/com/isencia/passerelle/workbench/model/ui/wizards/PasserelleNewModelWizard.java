ad294b9f74eaa82279446265b199fcf7774e3e97
heukoe <heukoe@384cdc12-9176-0410-81a5-8911e62acf96>
2010-04-19 07:56:26 +0000

    change package name from be.isencia to com.isencia

--- passerelle-tools/trunk/com.isencia.passerelle.workbench.model.ui/src/main/java/com/isencia/passerelle/workbench/model/ui/wizards/PasserelleNewModelWizard.java
+++ passerelle-tools/trunk/com.isencia.passerelle.workbench.model.ui/src/main/java/com/isencia/passerelle/workbench/model/ui/wizards/PasserelleNewModelWizard.java
@@ -133,7 +133,7 @@ public class PasserelleNewModelWizard extends Wizard implements INewWizard {
 			"<!DOCTYPE entity PUBLIC \"-//UC Berkeley//DTD MoML 1//EN\" \"http://ptolemy.eecs.berkeley.edu/xml/dtd/MoML_1.dtd\"> \r\n" +
             "<entity name=\"newModel\" class=\"ptolemy.actor.TypedCompositeActor\"> \r\n" +
             "   <property name=\"_createdBy\" class=\"ptolemy.kernel.attributes.VersionAttribute\" value=\"7.0.1\" /> \r\n" +
-            "   <property name=\"Director\" class=\"be.isencia.passerelle.domain.cap.Director\" > \r\n" +
+            "   <property name=\"Director\" class=\"com.isencia.passerelle.domain.cap.Director\" > \r\n" +
         	"      <property name=\"_location\" class=\"ptolemy.kernel.util.Location\" value=\"{20, 20}\" /> \r\n" +
             "   </property> \r\n" +
         	"</entity>";
