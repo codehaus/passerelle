70a0175aeccbde9d11de8599df34ecc28e8c245e
matt.gerring <matt.gerring@384cdc12-9176-0410-81a5-8911e62acf96>
2011-04-18 14:42:28 +0000

    Added parameter to TangoCommand which references a spec macro file directly and allows the substitution editor to be used on it. TODO Actually run this macro file through the spec interface.

--- passerelle-tools/trunk/com.isencia.passerelle.workbench.model/src/main/java/com/isencia/passerelle/workbench/model/actor/IPartListenerActor.java
+++ passerelle-tools/trunk/com.isencia.passerelle.workbench.model/src/main/java/com/isencia/passerelle/workbench/model/actor/IPartListenerActor.java
@@ -8,6 +8,12 @@ public interface IPartListenerActor {
 	 * Notify if a part associated with this actor is opened.
 	 * @param part
 	 */
+	public void partPreopen(ResourceObject ob);
+
+	/**
+	 * Notify if a part associated with this actor is opened.
+	 * @param part
+	 */
 	public void partOpened(IWorkbenchPart part, ResourceObject ob);
 
 }
