14792486d143673df8504435d2cb9dbfd92d090b
heukoe <heukoe@384cdc12-9176-0410-81a5-8911e62acf96>
2011-05-04 11:52:25 +0000

    add ActorType

--- passerelle-tools/trunk/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/properties/ActorGeneralSection.java
+++ passerelle-tools/trunk/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/properties/ActorGeneralSection.java
@@ -20,43 +20,46 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import ptolemy.kernel.util.NamedObj;
 
 import com.isencia.passerelle.workbench.model.editor.ui.editpart.AbstractBaseEditPart;
+import com.isencia.passerelle.workbench.model.editor.ui.palette.PaletteItemFactory;
 import com.isencia.passerelle.workbench.model.utils.ModelChangeRequest;
 import com.isencia.passerelle.workbench.model.utils.ModelUtils;
 
 public class ActorGeneralSection extends AbstractPropertySection {
-	
+
 	private NamedObj model = null;
 	private Text nameText;
-	private Text typeText;
-	
+	private CLabel typeText;
+
 	/**
 	 * Update model if properties have been changed
 	 */
 	private ModifyListener listener = new ModifyListener() {
 
 		public void modifyText(ModifyEvent event) {
-			if( model != null ) {
+			if (model != null) {
 				final String text = nameText.getText();
-				if( text != null ) {
-					ActorGeneralSection.setName(model, text);		               
+				if (text != null) {
+					ActorGeneralSection.setName(model, text);
 				}
 			}
 		}
 	};
-	
+
 	/**
 	 * Adds a name change event to the command stack.
 	 * 
-	 * This method will show an error message to the user if the name is not legal.
+	 * This method will show an error message to the user if the name is not
+	 * legal.
 	 * 
 	 * @param model
 	 * @param text
 	 */
 	public static void setName(final NamedObj model, final String text) {
-		
+
 		// Perform Change in a ChangeRequest so that all Listeners are notified
 		if (ModelUtils.isNameLegal(text)) {
-			model.requestChange(new ModelChangeRequest(ActorGeneralSection.class, model, "changeName") {
+			model.requestChange(new ModelChangeRequest(
+					ActorGeneralSection.class, model, "changeName") {
 				protected void _execute() throws Exception {
 					model.setDisplayName(text);
 					model.setName(text);
@@ -64,58 +67,88 @@ public class ActorGeneralSection extends AbstractPropertySection {
 			});
 		} else {
 			MessageDialog.openError(Display.getCurrent().getActiveShell(),
-					               "Invalid Name",
-					               "The name '"+text+"' is not allowed.\n\n"+
-					               "Names should not contain '.'");
+					"Invalid Name", "The name '" + text
+							+ "' is not allowed.\n\n"
+							+ "Names should not contain '.'");
 		}
 
 	}
-	
+
 	public void createControls(Composite parent, TabbedPropertySheetPage page) {
 		super.createControls(parent, page);
-		
-		Composite composite = getWidgetFactory().createFlatFormComposite(parent);
+
+		Composite composite = getWidgetFactory()
+				.createFlatFormComposite(parent);
 		FormData layoutData;
 		// Create Name TextInput Control
 		layoutData = new FormData();
 		nameText = getWidgetFactory().createText(composite, "");
 		layoutData.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
 		layoutData.right = new FormAttachment(100, 0);
-		layoutData.top = new FormAttachment(0,ITabbedPropertyConstants.VSPACE);
+		layoutData.top = new FormAttachment(0, ITabbedPropertyConstants.VSPACE);
 		nameText.setLayoutData(layoutData);
 		nameText.addModifyListener(listener);
-		
+
 		// Create Name Label Control
 		CLabel nameLabel = getWidgetFactory().createCLabel(composite, "Name:");
 		layoutData = new FormData();
 		layoutData.left = new FormAttachment(0, 0);
-		layoutData.right = new FormAttachment(nameText,-ITabbedPropertyConstants.HSPACE);
-		layoutData.top = new FormAttachment(nameText,0,SWT.CENTER);
+		layoutData.right = new FormAttachment(nameText,
+				-ITabbedPropertyConstants.HSPACE);
+		layoutData.top = new FormAttachment(nameText, 0, SWT.CENTER);
 		nameLabel.setLayoutData(layoutData);
+		
+		// Create Type Label Control
+		CLabel typeLabel = getWidgetFactory().createCLabel(composite, "Type:");
+		layoutData = new FormData();
+		layoutData.left = new FormAttachment(0, 0);
+		layoutData.top = new FormAttachment(nameLabel, 0);
+		typeLabel.setLayoutData(layoutData);
+		// Create Type Value Control
+		 typeText= getWidgetFactory().createCLabel(composite, "");
+		 layoutData = new FormData();
+		 layoutData.left = new FormAttachment(nameText,
+				 0, SWT.LEFT);
+		 layoutData.right = new FormAttachment(nameText,
+				 100, 0);
+		 layoutData.top = new FormAttachment(nameLabel, 0);
+		 typeText.setLayoutData(layoutData);
 	}
-	
+
 	/**
 	 * Get the model
 	 */
 	public void setInput(IWorkbenchPart part, ISelection selection) {
 		super.setInput(part, selection);
-		if( !(selection instanceof IStructuredSelection))
+		if (!(selection instanceof IStructuredSelection))
 			return;
-		Object input = ((IStructuredSelection)selection).getFirstElement();
-		if( input instanceof AbstractBaseEditPart ) {
-			model = (NamedObj) ((AbstractBaseEditPart)input).getEntity();
+		Object input = ((IStructuredSelection) selection).getFirstElement();
+		if (input instanceof AbstractBaseEditPart) {
+			model = (NamedObj) ((AbstractBaseEditPart) input).getEntity();
 		}
 	}
-	
+
 	/**
 	 * Set/Update properties based on model
 	 */
 	public void refresh() {
 		nameText.removeModifyListener(listener);
 		String name = "";
-		if( model != null )
+		if (model != null)
 			name = model.getName();
 		nameText.setText(name);
 		nameText.addModifyListener(listener);
+		if (typeText != null)
+			if (model != null) {
+				String type = PaletteItemFactory.get()
+						.getType(model.getClass());
+				if (type != null) {
+					typeText.setText(type);
+				} else {
+					typeText.setText(model.getClassName());
+				}
+			} else {
+				typeText.setText("Type unknown");
+			}
 	}
 }
