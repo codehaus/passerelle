b204b7c04526ac404c7f994ff430d8109438ede4
heukoe <heukoe@384cdc12-9176-0410-81a5-8911e62acf96>
2011-05-04 11:52:53 +0000

    add eclipse help

--- passerelle-tools/trunk/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/views/ActorAttributesView.java
+++ passerelle-tools/trunk/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/views/ActorAttributesView.java
@@ -21,6 +21,7 @@ import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.internal.help.WorkbenchHelpSystem;
 import org.eclipse.ui.part.ViewPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,138 +35,155 @@ import com.isencia.passerelle.actor.gui.PasserelleConfigurer;
 import com.isencia.passerelle.workbench.model.editor.ui.Activator;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.PasserelleModelMultiPageEditor;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.DeleteAttributeHandler;
+import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.HelpAttributeHandler;
 import com.isencia.passerelle.workbench.model.editor.ui.editpart.ActorEditPart;
 import com.isencia.passerelle.workbench.model.editor.ui.editpart.DirectorEditPart;
+import com.isencia.passerelle.workbench.model.editor.ui.palette.PaletteItemFactory;
 import com.isencia.passerelle.workbench.model.editor.ui.properties.ActorGeneralSection;
 import com.isencia.passerelle.workbench.model.editor.ui.properties.NamedObjComparator;
 import com.isencia.passerelle.workbench.model.ui.command.AttributeCommand;
 import com.isencia.passerelle.workbench.model.ui.utils.EclipseUtils;
 
-
 /**
- * Optional replacement for PropertiesView which renders the actor properties more simply
- * but with more customizable rules.
+ * Optional replacement for PropertiesView which renders the actor properties
+ * more simply but with more customizable rules.
  * 
  * @author gerring
- *
+ * 
  */
-public class ActorAttributesView extends ViewPart implements ISelectionListener, CommandStackEventListener {
+public class ActorAttributesView extends ViewPart implements
+		ISelectionListener, CommandStackEventListener {
 
-	private static Logger logger = LoggerFactory.getLogger(ActorAttributesView.class);
-	
+	private static Logger logger = LoggerFactory
+			.getLogger(ActorAttributesView.class);
+
 	public static final String ID = "com.isencia.passerelle.workbench.model.editor.ui.views.ActorAttributesView"; //$NON-NLS-1$
 
-	private TableViewer    viewer;
-	private NamedObj       actor;
-	private boolean        addedListener=false;
+	private TableViewer viewer;
+	private NamedObj actor;
+	private boolean addedListener = false;
 	private IWorkbenchPart part;
 
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		
-		this.part  = part;
-        if (updateSelection(selection))  return;
+
+		this.part = part;
+		if (updateSelection(selection))
+			return;
 		clear();
 	}
 
 	protected boolean updateSelection(final ISelection selection) {
-		
-		if (!(selection instanceof StructuredSelection)) return false;
-		
-		final Object sel = ((StructuredSelection)selection).getFirstElement(); 	
-		
-        if (sel instanceof ActorEditPart || sel instanceof DirectorEditPart) {
-			this.actor = sel instanceof ActorEditPart
-			           ? (NamedObj)((ActorEditPart)sel).getActor()
-					   : (NamedObj)((DirectorEditPart)sel).getDirectorModel();
-	
-			if (!addedListener && part instanceof PasserelleModelMultiPageEditor) {
-				((PasserelleModelMultiPageEditor)part).getEditor().getEditDomain().getCommandStack().addCommandStackEventListener(this);
-				addedListener = true;
+
+		if (!(selection instanceof StructuredSelection))
+			return false;
+
+		final Object sel = ((StructuredSelection) selection).getFirstElement();
+
+		if (sel instanceof ActorEditPart || sel instanceof DirectorEditPart) {
+			this.actor = sel instanceof ActorEditPart ? (NamedObj) ((ActorEditPart) sel)
+					.getActor()
+					: (NamedObj) ((DirectorEditPart) sel).getDirectorModel();
+
+			if (!addedListener
+					&& part instanceof PasserelleModelMultiPageEditor) {
+				// ((PasserelleModelMultiPageEditor)part).getEditor().getEditDomain().getCommandStack().addCommandStackEventListener(this);
+				// addedListener = true;
 			}
-	
+
 			final List<Parameter> parameterList = new ArrayList<Parameter>(7);
-			Iterator<Parameter> parameterIterator = actor.attributeList(Parameter.class).iterator();
+			Iterator<Parameter> parameterIterator = actor.attributeList(
+					Parameter.class).iterator();
 			while (parameterIterator.hasNext()) {
 				Parameter parameter = (Parameter) parameterIterator.next();
 				if (PasserelleConfigurer.isVisible(actor, parameter)) {
 					parameterList.add(parameter);
 				}
 			}
-	
-	        createTableModel(parameterList);	
-	        return true;
-        } 
-        
-        return false;
-    }
+
+			createTableModel(parameterList);
+			return true;
+		}
+
+		return false;
+	}
 
 	private void createTableModel(final List<Parameter> parameterList) {
-		
-		if (parameterList!=null) Collections.sort(parameterList, new NamedObjComparator());
-		
+
+		if (parameterList != null)
+			Collections.sort(parameterList, new NamedObjComparator());
+
 		viewer.setContentProvider(new IStructuredContentProvider() {
 			@Override
 			public void dispose() {
-				
+
 			}
+
 			@Override
-			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {}
+			public void inputChanged(Viewer viewer, Object oldInput,
+					Object newInput) {
+			}
 
 			@Override
 			public Object[] getElements(Object inputElement) {
-				if (parameterList==null || parameterList.isEmpty()) return new Parameter[]{};
-				final List<Object> ret = new ArrayList<Object>(parameterList.size()+1);
+				if (parameterList == null || parameterList.isEmpty())
+					return new Parameter[] {};
+				final List<Object> ret = new ArrayList<Object>(parameterList
+						.size() + 1);
 				ret.add(actor.getName());
 				ret.addAll(parameterList);
-				return	ret.toArray(new Object[ret.size()]);
+				return ret.toArray(new Object[ret.size()]);
 			}
 		});
-		
+
 		viewer.setInput(new Object());
 		viewer.refresh();
 	}
 
 	public void clear() {
-    	this.actor = null;
-    	if (part!=null && part instanceof PasserelleModelMultiPageEditor) {
-    	    ((PasserelleModelMultiPageEditor)part).getEditor().getEditDomain().getCommandStack().removeCommandStackEventListener(this);
-    	}
-        this.part  = null;
-    	this.addedListener=false;
+		this.actor = null;
+		if (part != null && part instanceof PasserelleModelMultiPageEditor) {
+			((PasserelleModelMultiPageEditor) part).getEditor().getEditDomain()
+					.getCommandStack().removeCommandStackEventListener(this);
+		}
+		this.part = null;
+		this.addedListener = false;
 		createTableModel(null);
 	}
 
-	
 	/**
 	 * Create contents of the view part.
+	 * 
 	 * @param parent
 	 */
 	@Override
 	public void createPartControl(Composite parent) {
-		
-		this.viewer = new TableViewer(parent, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
-		
+
+		this.viewer = new TableViewer(parent, SWT.SINGLE | SWT.H_SCROLL
+				| SWT.V_SCROLL | SWT.BORDER);
+
 		viewer.getTable().setLinesVisible(true);
 		viewer.getTable().setHeaderVisible(true);
-		
+
 		createColumns(viewer);
 		viewer.setUseHashlookup(true);
-		viewer.setColumnProperties(new String[]{"Property", "Value"});
-		
+		viewer.setColumnProperties(new String[] { "Property", "Value" });
+
 		createActions();
 		createPopupMenu();
-		
-		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(this);
-		
+
+		getSite().getWorkbenchWindow().getSelectionService()
+				.addSelectionListener(this);
+
 		viewer.getTable().addKeyListener(new KeyListener() {
-			
+
 			@Override
-			public void keyReleased(KeyEvent e) { }
-			
+			public void keyReleased(KeyEvent e) {
+			}
+
 			@Override
 			public void keyPressed(KeyEvent e) {
-				if (e.character==SWT.DEL) {
+				if (e.character == SWT.DEL) {
 					try {
 						deleteSelectedParameter();
 					} catch (IllegalActionException e1) {
@@ -174,7 +192,7 @@ public class ActorAttributesView extends ViewPart implements ISelectionListener,
 				}
 			}
 		});
-		
+
 		try {
 			this.part = EclipseUtils.getActivePage().getActiveEditor();
 			updateSelection(EclipseUtils.getActivePage().getSelection());
@@ -184,13 +202,15 @@ public class ActorAttributesView extends ViewPart implements ISelectionListener,
 	}
 
 	private void createColumns(TableViewer viewer) {
-		
-		final TableViewerColumn name   = new TableViewerColumn(viewer, SWT.LEFT, 0);
+
+		final TableViewerColumn name = new TableViewerColumn(viewer, SWT.LEFT,
+				0);
 		name.getColumn().setText("Property");
 		name.getColumn().setWidth(200);
 		name.setLabelProvider(new PropertyLabelProvider());
-		
-		final TableViewerColumn value   = new TableViewerColumn(viewer, SWT.LEFT, 1);
+
+		final TableViewerColumn value = new TableViewerColumn(viewer, SWT.LEFT,
+				1);
 		value.getColumn().setText("Value");
 		value.getColumn().setWidth(700);
 		value.setLabelProvider(new VariableLabelProvider(this));
@@ -209,12 +229,20 @@ public class ActorAttributesView extends ViewPart implements ISelectionListener,
 	 */
 	private void createPopupMenu() {
 		MenuManager menuMan = new MenuManager();
-		menuMan.add(new Action("Delete Attribute", Activator.getImageDescriptor("icons/delete_attribute.gif")) {
+		menuMan.add(new Action("Delete Attribute", Activator
+				.getImageDescriptor("icons/delete_attribute.gif")) {
 			public void run() {
 				(new DeleteAttributeHandler()).run(null);
 			}
 		});
-	    viewer.getControl().setMenu (menuMan.createContextMenu(viewer.getControl()));
+//		menuMan.add(new Action("Help", Activator
+//				.getImageDescriptor("icons/help.gif")) {
+//			public void run() {
+//				(new HelpAttributeHandler()).run(null);
+//			}
+//		});
+		viewer.getControl().setMenu(
+				menuMan.createContextMenu(viewer.getControl()));
 	}
 
 	@Override
@@ -223,11 +251,13 @@ public class ActorAttributesView extends ViewPart implements ISelectionListener,
 			viewer.getTable().setFocus();
 		}
 	}
-	
+
 	public void dispose() {
-		getSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(this);
-		if (part!=null && part instanceof PasserelleModelMultiPageEditor) {
-			((PasserelleModelMultiPageEditor)part).getEditor().getEditDomain().getCommandStack().removeCommandStackEventListener(this);
+		getSite().getWorkbenchWindow().getSelectionService()
+				.removeSelectionListener(this);
+		if (part != null && part instanceof PasserelleModelMultiPageEditor) {
+			((PasserelleModelMultiPageEditor) part).getEditor().getEditDomain()
+					.getCommandStack().removeCommandStackEventListener(this);
 		}
 		super.dispose();
 	}
@@ -241,34 +271,67 @@ public class ActorAttributesView extends ViewPart implements ISelectionListener,
 		return actor.getName();
 	}
 
+	public String getActorClass() {
+		if (actor == null){
+			return null;
+		}
+		return actor.getClass().getName();
+	}
+
 	public void setActorName(final String name) {
 		try {
 			ActorGeneralSection.setName(actor, name);
 		} catch (Exception ne) {
-			logger.error("Cannot set name as "+name, ne);
+			logger.error("Cannot set name as " + name, ne);
 		}
 	}
 
 	public void deleteSelectedParameter() throws IllegalActionException {
-		
+
 		final ISelection sel = viewer.getSelection();
-		if (sel!=null && sel instanceof StructuredSelection) {
-			final StructuredSelection s = (StructuredSelection)sel;
-			final Object              o = s.getFirstElement();
-			if (o instanceof String) return; // Cannot delete name
+		if (sel != null && sel instanceof StructuredSelection) {
+			final StructuredSelection s = (StructuredSelection) sel;
+			final Object o = s.getFirstElement();
+			if (o instanceof String)
+				return; // Cannot delete name
 			if (o instanceof Attribute) {
-				setAttributeValue(o,null);
+				setAttributeValue(o, null);
 			}
 		}
 	}
 
-	public void setAttributeValue(Object element, Object value) throws IllegalActionException {
-		
-		final PasserelleModelMultiPageEditor ed = (PasserelleModelMultiPageEditor)this.part;
-		final AttributeCommand              cmd = new AttributeCommand(viewer, element, value);
+	public void showHelpSelectedParameter() throws IllegalActionException {
+		// String langPrefix = "";
+		// if (Locale.getDefault().getLanguage() != null
+		// && !Locale.getDefault().getLanguage().equals(Locale.ENGLISH)) {
+		// langPrefix = Locale.getDefault().getLanguage() + "/";
+		// }
+		final ISelection sel = viewer.getSelection();
+		if (sel != null && sel instanceof StructuredSelection) {
+			final StructuredSelection s = (StructuredSelection) sel;
+			String actorClass = getActorClass();
+			if (actorClass != null) {
+				String href = "/"
+						+ PaletteItemFactory.get().getBuildId(actorClass)
+						+ "/html/" + getActorClass() + "_attributes.html";
+				// String href = "/"
+				// + PaletteItemFactory.get().getBuildId(actorName) + "/html/"
+				// + langPrefix + actorName + "_" + attr.getName() + ".html";
+				WorkbenchHelpSystem.getInstance().displayHelpResource(href);
+			}
+
+		}
+	}
+
+	public void setAttributeValue(Object element, Object value)
+			throws IllegalActionException {
+
+		final PasserelleModelMultiPageEditor ed = (PasserelleModelMultiPageEditor) this.part;
+		final AttributeCommand cmd = new AttributeCommand(viewer, element,
+				value);
 		ed.getEditor().getEditDomain().getCommandStack().execute(cmd);
 		ed.refreshActions();
-		
+
 	}
 
 }
